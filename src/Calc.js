248c281803c1a87d60ccb13b7d39c78bf5211cb0
sara <saraa379@yahoo.com>
2018-06-07 17:05:20 +0200

    Calc component is updated

---
+++ src/Calc.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import './Calc.css';
+
+class Calc extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '', clear: false, temp: 0 };
+		this.handleChange = this.handleChange.bind(this);
+		this.increase = this.increase.bind(this);
+		this.decrease = this.decrease.bind(this);
+		this.clear = this.clear.bind(this);
+	}
+
+	handleChange(event) {
+			var number = parseInt(event.target.value, 10);
+			if(isNaN(number)){
+					console.log("Input is not a number");
+					if(this.state.temp !== 0){
+							this.setState({value: this.state.temp});
+					}
+			} else {
+					this.setState({value: number});
+			}
+	}
+	increase(event) {
+			this.setState({clear: true});
+			if (this.state.value === '') {
+					this.setState({value: 1});
+			} else {
+					this.setState(prevState => {
+						 return {value: prevState.value + 1}
+					})
+			}
+	}
+	decrease(event) {
+			this.setState({clear: true});
+			if (this.state.value >= 1) {
+				this.setState(prevState => {
+					 return {value: prevState.value - 1}
+				})
+		  }
+	}
+	clear(event){
+		if (this.state.clear === true) {
+				this.setState({temp: this.state.value});
+				this.setState({value: ''});
+				this.setState({clear: false});
+		}
+	}
+
+	render() {
+		return (
+			<div className="calcWrap">
+					<h2>Component Räknare</h2>
+					<div className="formDiv">
+								<input type="text" value={this.state.value} onChange={this.handleChange} onClick={this.clear}/>
+								<div className="buttons">
+											<button onClick={this.increase}>+</button>
+											<button onClick={this.decrease}>-</button>
+								</div>
+					</div>
+			</div>
+		);//end of return
+	} //end of render
+
+}//end of class
+
+export default Calc;
---
+++ src/__tests__/Calc.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import Calc from '../Calc.js';
+
+
+describe('Calc component test suite', () => {
+
+	it('Smoke test: Calc component renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Calc />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	})
+	test('shallow smoke test', () => {
+		let wrapper = shallow(<Calc />);
+	})
+	test('initial state should be: value: empty string, clear: false, temp: 0', () => {
+		let wrapper = shallow(<Calc />);
+		expect( wrapper.state('value') ).toBe('');
+		expect( wrapper.state('clear') ).toBe(false);
+		expect( wrapper.state('temp') ).toBe(0);
+	})
+	test('Component contains h2', () => {
+		let wrapper = shallow(<Calc />);
+		const h2 = <h2>Component Räknare</h2>;
+		expect( wrapper.contains(h2)).toBe(true);
+	})
+
+/*
+	test('state from false to true when clicked', () => {
+		let wrapper = shallow(<OnOff />);
+		let button = wrapper.find('button').at(0);
+		button.simulate('click');
+		expect( wrapper.state('on') ).toBe( true );
+	})
+	test('state from true to false when clicked', () => {
+		let wrapper = shallow(<OnOff />);
+		wrapper.setState({ on: true });
+		let button = wrapper.find('button').at(0);
+		button.simulate('click');
+		expect( wrapper.state('on') ).toBe( false );
+	})*/
+
+
+})//end of Calc komponent test suite
+/*
+describe('Input field test suite', () => {
+		test('state from true to false when clicked', () => {
+				let wrapper = shallow(<Calc />);
+				wrapper.setState({ value: '' });
+				let inputWrap = wrapper.find('input').at(0);
+				input.simulate('change');
+				expect( wrapper.state('on') ).toBe( false );
+		})
+})*///end of input field test suite
+
+
+
+
+//
