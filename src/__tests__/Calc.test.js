8d80b5c72dee7781355d58f30077bbfc3a4a8e54
sara <saraa379@yahoo.com>
2018-06-12 13:39:18 +0200

    2 components are done

--- src/__tests__/Calc.test.js
+++ src/__tests__/Calc.test.js
@@ -4,61 +4,125 @@ import {shallow} from 'enzyme';
 import Calc from '../Calc.js';
 
 
-describe('Calc component test suite', () => {
+describe('Shallow rendering test suite', () => {
 
 	it('Smoke test: Calc component renders without crashing', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<Calc />, div);
 		ReactDOM.unmountComponentAtNode(div);
 	})
-	test('shallow smoke test', () => {
+	test('div calcWrap is present', () => {
 		let wrapper = shallow(<Calc />);
+		expect(wrapper.find('.calcWrap')).toHaveLength(1);
 	})
-	test('initial state should be: value: empty string, clear: false, temp: 0', () => {
+	it('Component contains h2', () => {
+		let wrapper = shallow(<Calc />);
+		const h2 = <h2>Räknare</h2>;
+		expect( wrapper.contains(h2)).toBe(true);
+	})
+	test('div formDiv is present', () => {
+		let wrapper = shallow(<Calc />);
+		expect(wrapper.find('.formDiv')).toHaveLength(1);
+	})
+	test('Input is present', () => {
+		let wrapper = shallow(<Calc />);
+		expect(wrapper.find('input')).toHaveLength(1);
+	})
+	test('2 button elements are present', () => {
+		let wrapper = shallow(<Calc />);
+		expect(wrapper.find('button')).toHaveLength(2);
+	})
+
+})//end of Calc komponent test suite
+
+describe('Events test suite', () => {
+
+	test('initial state test', () => {
 		let wrapper = shallow(<Calc />);
 		expect( wrapper.state('value') ).toBe('');
 		expect( wrapper.state('clear') ).toBe(false);
 		expect( wrapper.state('temp') ).toBe(0);
 	})
-	test('Component contains h2', () => {
-		let wrapper = shallow(<Calc />);
-		const h2 = <h2>Component Räknare</h2>;
-		expect( wrapper.contains(h2)).toBe(true);
+	test('state value: empty string to 1 when + button clicked', () => {
+			let wrapper = shallow(<Calc />);
+			let btnPlus = wrapper.find('button').at(0);
+			wrapper.setState({ value: '' });
+			btnPlus.simulate('click');
+			expect( wrapper.state('value') ).toBe(1);
 	})
-/*
-	test('Calc contains div .calcWrap', () => {
-	    let wrapper = shallow(<Calc />);
-	    expect(wrapper.find('.calcWrap').length).to.equal(1);
-	})*/
-
-
-/*
-	test('state from false to true when clicked', () => {
-		let wrapper = shallow(<OnOff />);
-		let button = wrapper.find('button').at(0);
-		button.simulate('click');
-		expect( wrapper.state('on') ).toBe( true );
+	test('state value: 1 to 2 when + button clicked', () => {
+			let wrapper = shallow(<Calc />);
+			let btnPlus = wrapper.find('button').at(0);
+			wrapper.setState({ value: 1 });
+			btnPlus.simulate('click');
+			expect( wrapper.state('value') ).toBe(2);
 	})
-	test('state from true to false when clicked', () => {
-		let wrapper = shallow(<OnOff />);
-		wrapper.setState({ on: true });
-		let button = wrapper.find('button').at(0);
-		button.simulate('click');
-		expect( wrapper.state('on') ).toBe( false );
-	})*/
-
-
+	test('state clear: false to true when - button clicked', () => {
+			let wrapper = shallow(<Calc />);
+			let btnMinus = wrapper.find('button').at(1);
+			wrapper.setState({ clear: false });
+			btnMinus.simulate('click');
+			expect( wrapper.state('clear') ).toBe(true);
+	})
+	test('state value: 1 to 0 when - button clicked', () => {
+			let wrapper = shallow(<Calc />);
+			let btnMinus = wrapper.find('button').at(1);
+			wrapper.setState({ value: 1 });
+			btnMinus.simulate('click');
+			expect( wrapper.state('value') ).toBe(0);
+	})
+	test('state value: 0 to 0 when - button clicked', () => {
+			let wrapper = shallow(<Calc />);
+			let btnMinus = wrapper.find('button').at(1);
+			wrapper.setState({ value: 0 });
+			btnMinus.simulate('click');
+			expect( wrapper.state('value') ).toBe(0);
+	})
+	test('state value: empty string to 1 when enters 1 in input field', () => {
+			let wrapper = shallow(<Calc />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: '' });
+			input.simulate('change', {target: {value: 1}});
+			expect( wrapper.state('value') ).toBe(1);
+	})
+	test('state value: 6 to 0 when enters string in input field', () => {
+			let wrapper = shallow(<Calc />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: 6 });
+			wrapper.setState({ temp: 0 });
+			input.simulate('change', {target: {value: ''}});
+			expect( wrapper.state('value') ).toBe(6);
+			expect( wrapper.state('temp') ).toBe(0);
+	})
+	test('state value: 6 to 89 when temp is 89 and enters string in input field', () => {
+			let wrapper = shallow(<Calc />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: 6 });
+			wrapper.setState({ temp: 89 });
+			input.simulate('change', {target: {value: ''}});
+			expect( wrapper.state('value') ).toBe(89);
+			expect( wrapper.state('temp') ).toBe(89);
+	})
+	test('if clear: true & value: 3 =>  clear: false & temp: 3', () => {
+			let wrapper = shallow(<Calc />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: 3 });
+			wrapper.setState({ clear: true });
+			input.simulate('click');
+			expect( wrapper.state('temp') ).toBe(3);
+			expect( wrapper.state('clear') ).toBe(false);
+	})
+	test('if clear: false => nothing changes', () => {
+			let wrapper = shallow(<Calc />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: 3 });
+			wrapper.setState({ clear: false });
+			input.simulate('click');
+			expect( wrapper.state('value') ).toBe(3);
+			expect( wrapper.state('clear') ).toBe(false);
+	})
 })//end of Calc komponent test suite
-/*
-describe('Input field test suite', () => {
-		test('state from true to false when clicked', () => {
-				let wrapper = shallow(<Calc />);
-				wrapper.setState({ value: '' });
-				let inputWrap = wrapper.find('input').at(0);
-				input.simulate('change');
-				expect( wrapper.state('on') ).toBe( false );
-		})
-})*///end of input field test suite
+
 
 
 
---
+++ src/__tests__/EditableList.test.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+import { mount } from 'enzyme';
+import { render } from 'enzyme';
+import EditableList from '../EditableList.js';
+
+
+describe('Shallow rendering test suite', () => {
+
+	it('Smoke test: Calc component renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<EditableList />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	})
+	test('div editWrap is present', () => {
+		let wrapper = shallow(<EditableList />);
+		expect(wrapper.find('.editWrap')).toHaveLength(1);
+	})
+	it('Component contains h2', () => {
+		let wrapper = shallow(<EditableList />);
+		const h2 = <h2>Redigerbar Lista</h2>;
+		expect( wrapper.contains(h2)).toBe(true);
+	})
+	test('div formEdit is present', () => {
+		let wrapper = shallow(<EditableList />);
+		expect(wrapper.find('.formEdit')).toHaveLength(1);
+	})
+	test('Input is present', () => {
+		let wrapper = shallow(<EditableList />);
+		expect(wrapper.find('input')).toHaveLength(1);
+	})
+	test('Button element btnAdd is present', () => {
+		let wrapper = shallow(<EditableList />);
+		expect(wrapper.find('#btnAdd')).toHaveLength(1);
+	})
+	test('Div listDiv is present when state.listEd has elements', () => {
+		let wrapper = shallow(<EditableList />);
+		wrapper.setState({ listEd: ['aaaa', 'bbbbb'] });
+		expect(wrapper.find('.listDiv')).toHaveLength(1);
+	})
+
+})//end of Calc komponent test suite
+
+describe('List rendering test suite', () => {
+
+	test('Div item is present when state.listEd has elements with right key', () => {
+	  let wrapper = mount(<EditableList />);
+		wrapper.setState({ listEd: ['aaaa', 'bbbbb'] });
+		expect(wrapper.find('.item')).toHaveLength(2);
+		expect(wrapper.find('.item').at(1).key()).toBe('1');
+	})
+	test('p is present when state.listEd has elements', () => {
+		let wrapper = mount(<EditableList />);
+		wrapper.setState({ listEd: ['aaaa', 'bbbbb'] });
+		expect(wrapper.find('p')).toHaveLength(2);
+	})
+	test('Button delete is present when state.listEd has elements', () => {
+		let wrapper = mount(<EditableList />);
+		wrapper.setState({ listEd: ['aaaa', 'bbbbb'] });
+		expect(wrapper.find('.delete')).toHaveLength(2);
+	})
+
+
+})//end of Calc komponent test suite
+
+describe('Events test suite', () => {
+
+	test('initial state test', () => {
+		let wrapper = shallow(<EditableList />);
+		expect( wrapper.state('value') ).toBe('');
+		expect( wrapper.state('listEd') ).toHaveLength(0);
+	})
+	test('state value: empty string to 1 when enters 1 in input field', () => {
+			let wrapper = shallow(<EditableList />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: '' });
+			input.simulate('change', {target: {value: 1}});
+			expect( wrapper.state('value') ).toBe(1);
+	})
+	test('state value: 7 to m when enters m in input field', () => {
+			let wrapper = shallow(<EditableList />);
+			let input = wrapper.find('input').at(0);
+			wrapper.setState({ value: '7' });
+			input.simulate('change', {target: {value: 'm'}});
+			expect( wrapper.state('value') ).toBe('m');
+	})
+	test('btnAdd adds input value into listEd when clicked', () => {
+			let wrapper = shallow(<EditableList />);
+			let btnAdd = wrapper.find('button').at(0);
+			wrapper.setState({ value: 'aaaa' });
+			btnAdd.simulate('click');
+			expect( wrapper.state('value') ).toBe('');
+			expect( wrapper.state('listEd') ).toEqual(['aaaa']);
+	})
+	test('btnAdd pressed when input value is empty => listEd unchanged', () => {
+			let wrapper = shallow(<EditableList />);
+			let btnAdd = wrapper.find('button').at(0);
+			wrapper.setState({ value: '' });
+			btnAdd.simulate('click');
+			expect( wrapper.state('value') ).toBe('');
+			expect( wrapper.state('listEd') ).toEqual([]);
+	})
+	test('btnDelete removes first item from lisEd', () => {
+			let wrapper = mount(<EditableList />);
+			wrapper.setState({ listEd: ['aaaa', 'bbbb'] });
+			let btnDelete = wrapper.find('.delete').at(0);
+			btnDelete.simulate('click');
+			expect( wrapper.state('listEd') ).toEqual(['bbbb']);
+	})
+	test('btnDelete removes second item from lisEd', () => {
+			let wrapper = mount(<EditableList />);
+			wrapper.setState({ listEd: ['aaaa', 'bbbb'] });
+			let btnDelete = wrapper.find('.delete').at(1);
+			btnDelete.simulate('click');
+			expect( wrapper.state('listEd') ).toEqual(['aaaa']);
+	})
+
+})//end of Calc komponent test suite
+
+
+
+
+
+//
